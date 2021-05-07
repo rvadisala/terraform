09c875b7b98705196e7ae4f70a187d58d6569fa2
rvadisala <ravi.vadisala@yahoo.co.in>
2021-05-07 11:42:44 +0530

    Update provider.tf

--- demo-1/provider.tf
+++ demo-1/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                  = "ap-south-1"
   shared_credentials_file = "/Users/rvadisala/.aws/credentials"
-  profile                 = "mfa"
+  profile                 = ""
 }
